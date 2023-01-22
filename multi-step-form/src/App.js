b653bdaa1942df42be76d59da15633bd60ae38b7
Ahmet Sait Ünlü <ahmetsait239@gmail.com>
2023-01-22 14:37:24 +0300

    multi step form starter

---
+++ multi-step-form/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import SideBar from './component/SideBar';
+import PersonalInfo from './pages/PersonalInfo';
+import SelectPlan from './pages/SelectPlan';
+import AddOns from './pages/AddOns';
+import Finishing from './pages/Finishing';
+
+function App() {
+  return (
+    <div>
+      <SideBar />
+      <Routes>
+        <Route path='/' element={<PersonalInfo />} />
+        <Route path='SelectPlan' element={<SelectPlan/>} />
+        <Route path='AddOns' element={<AddOns/>} />
+        <Route path='Finishing' element={<Finishing />} />
+      </Routes>
+    </div>
+
+  );
+}
+
+export default App;
